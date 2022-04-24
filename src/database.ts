34ff0b8fce447e6e241561edcc3cb24b97a81fd6
OmarMJ <omarmitma123@gmail.com>
2022-04-23 21:13:33 -0500

    api v1

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,7 @@
 import { connect } from 'mongoose';
+import config from './config'
 
 export async function startConnection(){
-    await connect('mongodb://localhost/challenge');
+    await connect(config.mongodbURL || "");
     console.log("Database is connected")
 }
